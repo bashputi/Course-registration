33677df91f8988e0f4b52e5b4cba97ed3ae8d31a
Rime <rimeislam672@gmail.com>
2023-09-15 19:53:16 +0600

    design done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,27 +1,44 @@
+import { useState } from 'react';
 import './Home.css'
+import { useEffect } from 'react';
 
 const Home = () => {
+     
+    const [allCourses, setAllCourses] = useState([]);
+
+
+    useEffect(() => {
+        fetch("./data.json")
+        .then((res) => res.json())
+        .then((data) => setAllCourses(data));
+    }, []);
+
+
     return (
         <div>
             <div className='container'>
-                <div className='card-container'>
-                    <div className='card'>
-                       <div className='main-part'>
-                       <div className='card-img'>
-                            <img src="https://i.ibb.co/12yvRR7/Rectangle-3.png" alt="" />
+              <div className="home-container">
+              <div className='card-container'>
+                    {
+                        allCourses.map((course) => (<div key={course.id} className='card'>
+                        <div className='main-part'>
+                        <div className='card-img'>
+                             <img src="https://i.ibb.co/12yvRR7/Rectangle-3.png" alt="" />
+                         </div>
+                         <h3 className='name'>introdution</h3>
+                         <p>hhhhhhhhh</p>
+                         <div className="info">
+                             <h2>Price</h2>
+                             <h2>Credit</h2>
+                         </div>
+                         <button className='btn'>Select</button>
                         </div>
-                        <h3 className='name'>introdution</h3>
-                        <p>hhhhhhhhh</p>
-                        <div className="info">
-                            <h2>Price</h2>
-                            <h2>Credit</h2>
-                        </div>
-                        <button className='btn'>Select</button>
-                       </div>
-                    </div>
+                     </div>))
+                    }
             
                 </div>
-                <div className='credit'>
+               <div>
+               <div className='credit'>
                     <h3>Credit Hour Remaining</h3>
                     <hr />
                     <h3>Course Name</h3>
@@ -30,6 +47,8 @@ const Home = () => {
                     <hr />
                     <h5>Total Price</h5>
                 </div>
+               </div>
+              </div>
             </div>
         </div>
     );
